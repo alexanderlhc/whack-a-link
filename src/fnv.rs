e4f0a3029b7b30c3a691b3376b5181b85b751a3e
Alexander <alexanderlhc@disroot.org>
2024-03-23 10:24:22 +0100

    32bit fnv

--- src/fnv.rs
+++ src/fnv.rs
@@ -2,22 +2,23 @@ pub const PRIME_32: u32 = 16777619;
 pub const OFFSET_32: u32 = 2166136261;
 
 pub trait FnvHash {
-    fn hash(&self, data: &str) -> u64;
+    fn hash(&self, data: &str) -> u32;
 }
 
 pub struct Fnv {
-    pub prime: u64,
-    pub offset: u64,
+    pub prime: u32,
+    pub offset: u32,
 }
 
 impl FnvHash for Fnv {
-    fn hash(&self, data: &str) -> u64 {
-        let mut hash: u64 = self.offset;
+    fn hash(&self, data: &str) -> u32 {
+        let mut hash: u32 = self.offset;
 
         for byte in data.bytes() {
-            hash ^= byte as u64;
+            hash ^= byte as u32;
             hash = hash.wrapping_mul(self.prime);
         }
-        hash as u64
+        hash as u32
     }
 }
+
