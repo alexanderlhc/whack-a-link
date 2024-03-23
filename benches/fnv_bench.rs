621e8520761c3e97b505cf87eac504c63e84e8a0
Alexander <alexanderlhc@disroot.org>
2024-03-23 10:40:17 +0100

    bench with fake data

---
+++ benches/fnv_bench.rs
@@ -0,0 +1,26 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use fake::faker::lorem::en::Sentence;
+use fake::Fake;
+use whack_a_link::fnv::{Fnv, FnvHash, OFFSET_32, PRIME_32};
+
+fn bench_fnv(c: &mut Criterion) {
+    let fnv = Fnv {
+        prime: PRIME_32,
+        offset: OFFSET_32,
+    };
+    // bench
+    let mut group = c.benchmark_group("FNV");
+
+    for i in 1..=5 {
+        let sentence: String = Sentence(1..i + 1).fake();
+
+        group.bench_function(BenchmarkId::new("fnv.hash", i), |b| {
+            b.iter(|| fnv.hash(&sentence))
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_fnv);
+criterion_main!(benches);
