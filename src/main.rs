24c11500ac07a9ed904ade4f789a7df6be489736
Alexander <alexanderlhc@disroot.org>
2024-03-23 10:40:06 +0100

    hello hash

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-mod fnv;
-use fnv::Fnv;
-
-use crate::fnv::{FnvHash, OFFSET_32, PRIME_32};
+use whack_a_link::fnv::{Fnv, FnvHash, OFFSET_32, PRIME_32};
 
 fn main() {
     let fnv = Fnv {
-        prime: PRIME_32 as u64,
-        offset: OFFSET_32 as u64,
+        prime: PRIME_32,
+        offset: OFFSET_32,
     };
 
     let data = "hello";
