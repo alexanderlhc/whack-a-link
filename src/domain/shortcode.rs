313da231ef7abdf441941c3b0ba8dd115d9775ac
Alexander <alexanderlhc@disroot.org>
2024-03-26 22:29:00 +0100

    added tests for shortcode

--- src/domain/shortcode.rs
+++ src/domain/shortcode.rs
@@ -17,3 +17,40 @@ impl Hash for ShortCode {
         format!("{:x}", hashed)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_short_code() {
+        let short_code = ShortCode("hello".to_string());
+        assert_eq!(short_code.compress(), "4f9f2cab");
+    }
+
+    #[test]
+    fn handles_empty_string() {
+        let short_code = ShortCode("".to_string());
+        assert_eq!(short_code.compress(), "811c9dc5");
+    }
+
+    #[test]
+    fn handles_unicode() {
+        let short_code = ShortCode("😀".to_string());
+        assert_eq!(short_code.compress(), "33a29608");
+    }
+
+    #[test]
+    fn handles_long_string() {
+        let short_code = ShortCode(
+            "hello world, how are you today? tired of running meaningless tests?".to_string(),
+        );
+        assert_eq!(short_code.compress(), "e21c971c");
+    }
+
+    #[test]
+    fn handles_odd_characters() {
+        let short_code = ShortCode("!#¤%&/()=?".to_string());
+        assert_eq!(short_code.compress(), "7c85c4ca");
+    }
+}
