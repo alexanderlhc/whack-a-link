80aa4b3a09447abccbc5c1500d19d9429bdaa0be
Alexander <alexanderlhc@disroot.org>
2024-03-26 20:26:18 +0100

    shorten method added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use whack_a_link::fnv::{Fnv, FnvHash, OFFSET_32, PRIME_32};
 
-fn main() {
-    let fnv = Fnv {
-        prime: PRIME_32,
-        offset: OFFSET_32,
-    };
+const FNV: Fnv = Fnv {
+    prime: PRIME_32,
+    offset: OFFSET_32,
+};
 
+fn main() {
     let data = "hello";
-    let hash = fnv.hash1a(data);
-    println!("Hash for {} is {}", data, hash);
+    let res = shorten(data);
+    println!("Shortened: {}", res)
+}
+
+pub fn shorten(text: &str) -> String {
+    let hashed = FNV.hash1a(text);
+    println!("Hashed: {}", hashed);
+    let hex = format!("{:x}", hashed);
+    hex
 }
