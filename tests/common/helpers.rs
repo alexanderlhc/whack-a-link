4723e1fc92027a0eeb1e5a4c4acd1414fc15e7f6
Alexander Christensen <alexanderlhc@disroot.org>
2024-04-06 18:22:22 +0200

    testwebapp expose .get for http GET calls

--- tests/common/helpers.rs
+++ tests/common/helpers.rs
@@ -1,3 +1,4 @@
+use reqwest::Response;
 use whack_a_link::webapp::webapp::{Config, WebApp};
 
 pub struct TestWebApp {
@@ -12,4 +13,11 @@ pub async fn run_server() -> TestWebApp {
     tokio::spawn(async move { webapp.server.await.unwrap() });
 
     TestWebApp { address }
+}
+
+impl TestWebApp {
+    pub async fn get(&self, path: &str) -> Response {
+        let url = format!("{}{}", self.address, path);
+        reqwest::get(url).await.unwrap()
+    }
 }
--- tests/health_check_test.rs
+++ tests/health_check_test.rs
@@ -7,11 +7,10 @@ mod common;
 #[tokio::test]
 async fn test_health_check_exist() {
     let test_server = run_server().await;
-    let url = format!("{}/health", test_server.address);
 
-    let req = reqwest::get(url).await.unwrap();
-    let status = req.status();
-    let body = req.text().await.unwrap();
+    let res = test_server.get("/health").await;
+    let status = res.status();
+    let body = res.text().await.unwrap();
 
     assert_eq!(body, "OK");
     assert_eq!(status, StatusCode::OK);
