99c20329a8a80058049d6651b537283c3289d690
Alexander Christensen <alexanderlhc@disroot.org>
2024-04-06 18:14:55 +0200

    creates a testwebapp helper

---
+++ tests/common/helpers.rs
@@ -0,0 +1,15 @@
+use whack_a_link::webapp::webapp::{Config, WebApp};
+
+pub struct TestWebApp {
+    pub address: String,
+}
+
+pub async fn run_server() -> TestWebApp {
+    let port = "0".to_string(); // 0 assigns random port
+    let config = Config { port };
+    let webapp = WebApp::create_app(config).await.unwrap();
+    let address = format!("http://localhost:{}", webapp.port);
+    tokio::spawn(async move { webapp.server.await.unwrap() });
+
+    TestWebApp { address }
+}
