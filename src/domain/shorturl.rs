d8fb3523ef0aeb45f6c34a3bcccdd3019af253e7
Alexander <alexanderlhc@disroot.org>
2024-04-28 21:16:28 +0200

    remove unused code

--- src/domain/shorturl.rs
+++ src/domain/shorturl.rs
@@ -23,13 +23,6 @@ impl ShortUrl {
     }
 }
 
-impl ShortUrl {
-    pub fn from_shortcode(base_url: &str, shortcode: &str) -> Url {
-        todo!()
-        // Url::parse(&format!("{}/{}", base_url, shortcode)).unwrap()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
